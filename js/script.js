fe3326a9c85cbe16e019a2fa9fc227287e084a87
BarbonFisso <barbieri.mat@hotmail.it>
2021-10-20 14:50:04 +0200

    minor correction

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // chiedi all'utente nome, cognome, colore preferito e genera una password con quelle informazioni e 21 alla fine
 
 // chiedo all'utente il nome
-const nome = prompt("Qual è il tuo nome?")
+const nome = prompt("Qual è il tuo nome?");
 
 // chiedo all'utente il cognome
-const cognome = prompt("Qual è il tuo cognome?")
+const cognome = prompt("Qual è il tuo cognome?");
 
 // chiedo all'utente il colore preferito
-const colore = prompt("Qual è il tuo colore preferito?")
+const colore = prompt("Qual è il tuo colore preferito?");
 
 // scrivo in pagina la password generata
-document.getElementById("password").innerHTML = `La tua password è ${nome}${cognome}${colore}21`
+document.getElementById("password").innerHTML = `La password assegnata è ${nome}${cognome}${colore}21`;
